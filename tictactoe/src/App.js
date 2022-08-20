051691c3dd26d169fbcc02b1819e06caa0a51f5a
Exist95 <96723716+Exist95@users.noreply.github.com>
2022-08-20 18:23:24 +0900

    TicTacToe

---
+++ tictactoe/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import TicTacToe from "./TicTacToe/TicTacToe";
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+};
+
+export default App;
